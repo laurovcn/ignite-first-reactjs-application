710981282a86f0b46cca497e310aef5886989caa
lvneto <lauro.neto.1995@gmail.com>
2022-01-18 12:09:55 -0300

    refactoring directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import { GlobalStyle } from "./styles/global";
-
 import {
   Route,
   BrowserRouter,
   Routes
 } from "react-router-dom";
-import { Login } from './pages/login';
-import Home from './pages/Home';
+
+import { Login } from './pages/Login';
+import { Home } from './pages/Home';
 
 export const App = () => {
 
-  return (   
-    <>
+  return ( 
+
       <BrowserRouter>  
         <Routes>
           <Route path="/" element={<Login/>} /> 
-          <Route path="/home" element={<Home/>} /> 
+          <Route path="/Dashboard" element={<Home/>} /> 
         </Routes>          
-      </BrowserRouter>      
-      <GlobalStyle />
-     </>
+      </BrowserRouter>   
   );
 }
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import { Dashboard } from '../components/Dashboard';
-import { Header } from '../components/Header';
-import { NewTransactionModal } from '../components/NewTransactionModal';
-import { TransactionsProvider } from '../hooks/useTransactions';
-import { GlobalStyle } from '../styles/global';
+import { Dashboard } from '../../components/Dashboard';
+import { Header } from '../../components/Header';
+import { NewTransactionModal } from '../../components/NewTransactionModal';
+import { TransactionsProvider } from '../../hooks/useTransactions';
+import { GlobalStyle } from '../../styles/global';
 
 export function Home() { 
 
@@ -18,14 +18,13 @@ function handleOpenNewTransactionModal(){
 
   return (
     <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/> 
       <NewTransactionModal 
         isOpen ={isNewTransactionModalOpen}
-        onRequestClose={handleCloseNewTransactionModal}/>
-        <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/> 
+        onRequestClose={handleCloseNewTransactionModal}/>        
       <Dashboard />
       <GlobalStyle />
    </TransactionsProvider>
   );
 }
 
-export default Home;
